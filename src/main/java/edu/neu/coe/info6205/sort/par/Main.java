7562a637855604630fea0bc4fa101f021df1ace9
Chaman Betrabet <113218344+chamanbetra@users.noreply.github.com>
2023-02-15 20:47:44 -0500

    Assignment 5

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -15,16 +15,16 @@ import java.util.concurrent.ForkJoinPool;
  * TODO tidy it up a bit.
  */
 public class Main {
-    public static int threadCount = 2;
+    public static int threadCount = 16;
     public static ForkJoinPool myPool = new ForkJoinPool(threadCount);
     public static void main(String[] args) {
         processArgs(args);
         System.out.println("Degree of parallelism: " + myPool.getParallelism());
         Random random = new Random();
-        int[] array = new int[1000000];
+        int[] array = new int[16000000];
         ArrayList<Long> timeList = new ArrayList<>();
         for (int j = 0; j < 20; j++) {
-            ParSort.cutoff = 50000 * (j + 1);
+            ParSort.cutoff = 800000 * (j + 1);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
